18c73b2064d4d99ce4cafdfcab2becf2671f27e4
daniel anggrianto <daniela@aweber.com>
2014-09-30 11:28:49 -0400

    node.sh: add node.sh

--- node-chrome/node.sh
+++ node-chrome/node.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
+set -e
+su chrome <<'EOF'
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1366x768x16 &
-
-x11vnc -passwd secret -display :99 -N -forever &
-
-java -Djava.security.egd=file:/dev/random \
--jar /opt/selenium-server.jar \
--role node \
--hubHost ${HUB_PORT_4444_TCP_ADDR} \
--hubPort ${HUB_PORT_4444_TCP_PORT} \
--browser browserName=firefox,maxInstances=1,platform=LINUX
+java -jar /home/chrome/selenium-server.jar -role node -hubHost ${HUB_PORT_4444_TCP_ADDR} -hubPort ${HUB_PORT_4444_TCP_PORT}  -browser browserName=chrome,maxInstances=1,platform=LINUX
+EOF
